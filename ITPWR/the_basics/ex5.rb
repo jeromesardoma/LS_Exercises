663cd6692db617553d4bcf2906f45b91aa99f599
Jerome Sardoma <jeromesardoma@gmail.com>
2020-06-03 12:20:43 -0700

    completed the basics exercises 5 and 6

---
+++ ITPWR/the_basics/ex5.rb
@@ -0,0 +1,4 @@
+8_factorial = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+7_factorial = 7 * 6 * 5 * 4 * 3 * 2 * 1
+6_factorial = 6 * 5 * 4 * 3 * 2 * 1
+5_factorial = 5 * 4 * 3 * 2 * 1
---
+++ ITPWR/the_basics/ex6.rb
@@ -0,0 +1,7 @@
+float1 = 3.4
+float2 = 4.3
+float3 = 12.5
+
+puts float1 ** 2
+puts float2 ** 2
+puts float3 ** 2
