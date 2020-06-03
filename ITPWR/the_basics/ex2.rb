80e170ea8fd45ab0fdfd278bb331bb611327d495
Jerome Sardoma <jeromesardoma@gmail.com>
2020-06-03 10:17:24 -0700

    the basics exercises 1-4

---
+++ ITPWR/the_basics/ex2.rb
@@ -0,0 +1,6 @@
+number = 2453
+
+thousands_digit = number / 1000
+hundreds_digit = ( number / 100 ) % 10 
+tens_digit =  ( number / 10 ) % 10
+ones_digit = number % 10
---
+++ ITPWR/the_basics/ex4.rb
@@ -0,0 +1,10 @@
+movies = {
+	movie1: 1975,
+	movie2: 2004,
+	movie3: 2013,
+	movie4: 2001,
+	movie5: 1981
+}
+
+a = [1975, 2004, 2013, 2001, 1981]
+a.each{ |y| puts y }
