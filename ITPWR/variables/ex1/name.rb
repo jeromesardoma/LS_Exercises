06234524dfdcf53eda8e9760fa6240778a7dd1c1
Jerome Sardoma <jeromesardoma@gmail.com>
2020-06-03 16:31:56 -0700

    variables exercises complete

---
+++ ITPWR/variables/ex1/name.rb
@@ -0,0 +1,17 @@
+# exercise 1
+
+puts "Type in your name: "
+name = gets.chomp
+puts "Hey #{name}!"
+
+# exercise 3
+
+10.times{ puts name }
+
+# exercise 4
+
+puts "Type in your first name: "
+first_name = gets.chomp
+puts "Type in your last name: "
+last_name = gets.chomp
+puts "Hello #{first_name} #{last_name}!"
