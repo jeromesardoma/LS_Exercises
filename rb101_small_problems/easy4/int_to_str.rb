0a904e10dad77dea2ee2f78e771c5e14913ff82f
Jerome Sardoma <jeromesardoma@gmail.com>
2020-06-27 13:19:53 -0700

    finished easy4 exercises

--- rb101_small_problems/easy4/int_to_str.rb
+++ rb101_small_problems/easy4/int_to_str.rb
@@ -27,3 +27,16 @@ def int_to_str_v2(int)
 end
 
 p int_to_str_v2 46543501948
+
+def signed_int_to_str(int)
+  x = if int < 0
+        { sign: '-', value: -int }
+      else
+        { sign: '+', value: int }
+      end
+  "#{x[:sign]}#{int_to_str(x[:value])}"
+end
+
+p signed_int_to_str 53223
+p signed_int_to_str -53223
+
